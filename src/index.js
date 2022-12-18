2ff3c044cb6f1f4633bdc6cfa86dde1f9ecf1d6c
Scott <55364794+scottroot@users.noreply.github.com>
2022-12-18 01:22:37 -0500

    add text/html screenshots

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import got from 'got';
+// import got from 'got';
 import fs from "fs";
-import { fileTypeFromStream } from 'file-type';
+// import { fileTypeFromStream } from 'file-type';
 import imageThumbnail from 'image-thumbnail';
 import express from "express";
 import nodecache from 'node-cache';
 import ffmpeg from "fluent-ffmpeg";
 import ffmpeg_static from "ffmpeg-static";
+import axios from "axios";
+import puppeteer from "puppeteer";
 
+const getContentType = (url) => (
+  axios.get(url)
+    .then((res) => res.headers["content-type"])
+    .then((val) => String(val).split(";")[0]
+));
+
+const getPageScreenshot = (url) => puppeteer
+    .launch({
+      defaultViewport: {
+        width: 500,
+        height: 600,
+      },
+    })
+    .then(async (browser) => {
+      const page = await browser.newPage();
+      await page.goto(url, {waitUntil: 'networkidle0'});
+      const data = await page.screenshot({encoding: 'base64'});
+      await browser.close();
+      return data
+    })
+;
 
 const appCache = new nodecache({ stdTTL : 3599});
 var app = express();
@@ -20,29 +43,34 @@ app.get('/favicon.ico', async(req,res) => {
   return res.send("404")
 })
 
+app.get('/page/:id', async(req,res) => {
+  const id = req.params.id;
+  let url = id;
+  if (id.length === 43) {
+    url = `https://arweave.net/${id}`;
+  }
+  return res.send(await getPageScreenshot(url));
+})
 
 
 app.get('/:id', async(req,res) => {
   const id = req.params.id;
-  if(!id || id.length !== 43) return res.send("incorrect id format");
-
-
+  // if(!id || id.length !== 43) return res.send("incorrect id format");
   if(appCache.has(`${id}`)){
     console.log('Get data from Node Cache');
     const data = appCache.get(`${id}`);
     return res.send(data);
   }
   else {
-    const url = `https://arweave.net/${id}`;
-    console.log(`Url = ${url}`);
-    const stream = got.stream(url);
-    const dataType = await fileTypeFromStream(stream)
-    console.log(dataType);
-    const mimeType = String(dataType.mime).split("/")[0];
+    let url = id;
+    if(id.length === 43) {
+      url = `https://arweave.net/${id}`;
+    }
 
+    const contentType = await getContentType(url);
+    const mimeType = contentType.split("/")?.slice(0,1)[0];
     try {
-      if(mimeType === "video" || dataType.mime === "image/gif") {
-        console.log('Fetch data from video API');
+      if(mimeType === "video" || contentType === "image/gif") {
         var outStream = fs.createWriteStream('video.mp4');
         await ffmpeg(url)
           .setFfmpegPath(ffmpeg_static)
@@ -57,7 +85,7 @@ app.get('/:id', async(req,res) => {
           })
           .takeScreenshots({
               count: 1,
-              timemarks: [ '0' ],
+              timemarks: [ '5' ],
               filename: "thumbnail.png"
             }, 'tmp')
           .pipe(outStream, { end: true });
@@ -65,11 +93,15 @@ app.get('/:id', async(req,res) => {
         var img64 = new Buffer.from(bitmap, "binary").toString('base64');
         const data = `data:image/png;base64,${img64}`;
         appCache.set(`${id}`,data);
-        // console.log(`Video data = ${data}`)
+        return res.send(`${data}`);
+      }
+      if(contentType === "text/html") {
+        const screenshot64 = await getPageScreenshot(url);
+        console.log(screenshot64);
+        const data = `data:image/png;base64,${screenshot64}`;
         return res.send(`${data}`);
       }
       else {
-        console.log('Fetch data from image API');
         const thumbnail = await imageThumbnail({ uri: url }, {width: 320, height: 320});
         const img64 = thumbnail.toString('base64');
         const data = `data:image/png;base64,${img64}`;
@@ -78,7 +110,6 @@ app.get('/:id', async(req,res) => {
       }
     } catch (err) {console.log(err.message)}
   }
-  console.log(`No data = "none"`)
   return res.send("none");
 });
 
