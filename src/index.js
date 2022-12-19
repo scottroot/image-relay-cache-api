13503975dadb26e7df732ff70f334966b92a5a33
Scott <55364794+scottroot@users.noreply.github.com>
2022-12-19 18:20:06 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// import got from 'got';
 import fs from "fs";
 // import { fileTypeFromStream } from 'file-type';
 import imageThumbnail from 'image-thumbnail';
@@ -60,69 +59,73 @@ app.get('/page/:id', async(req,res) => {
 })
 
 app.get('/:id', async(req,res) => {
-  const id = req.params.id;
-  // if(!id || id.length !== 43) return res.send("incorrect id format");
-  if(appCache.has(`${id}`)){
-    console.log('Get data from Node Cache');
-    const data = appCache.get(`${id}`);
-    return res.send(data);
-  }
-  else {
-    let url = id;
-    if(id.length === 43) {
-      url = `https://arweave.net/${id}`;
-    }
-    if(!url.startsWith("http")) {
-      url = `https://${url}`;
-    }
+  try {
+    const id = req.params.id;
+    // if(!id || id.length !== 43) return res.send("incorrect id format");
+    if (appCache.has(`${id}`)) {
+      console.log('Get data from Node Cache');
+      const data = appCache.get(`${id}`);
+      return res.send(data);
+    } else {
+      let url = id;
+      if (id.length === 43) {
+        url = `https://arweave.net/${id}`;
+      }
+      if (!url.startsWith("http")) {
+        url = `https://${url}`;
+      }
 
-    const contentType = await getContentType(url);
-    const mimeType = contentType.split("/")?.slice(0,1)[0];
-    try {
-      if(mimeType === "video" || contentType === "image/gif") {
-        let rname = (Math.random() + 1).toString(36).substring(7);
-        var outStream = fs.createWriteStream('video.mp4');
-        await ffmpeg(url)
-          .setFfmpegPath(ffmpeg_static)
-          .format('mjpeg')
-          .frames(1)
-          .size('320x320')
-          .on('error', function(err) {
-            console.log('An error occurred: ' + err.message);
-          })
-          .on('end', function() {
-            console.log('Processing finished !');
-          })
-          .takeScreenshots({
+      const contentType = await getContentType(url);
+      const mimeType = contentType.split("/")?.slice(0, 1)[0];
+      try {
+        if (mimeType === "video" || contentType === "image/gif") {
+          let rname = (Math.random() + 1).toString(36).substring(7);
+          var outStream = fs.createWriteStream('video.mp4');
+          await ffmpeg(url)
+            .setFfmpegPath(ffmpeg_static)
+            .format('mjpeg')
+            .frames(1)
+            .size('320x320')
+            .on('error', function (err) {
+              console.log('An error occurred: ' + err.message);
+            })
+            .on('end', function () {
+              console.log('Processing finished !');
+            })
+            .takeScreenshots({
               count: 1,
-              timemarks: [ '5' ],
+              timemarks: ['5'],
               filename: `thumbnail${rname}.png`
             }, 'tmp')
-          .pipe(outStream, { end: true });
-        var bitmap = await fs.readFileSync("tmp/thumbnail.png");
-        var img64 = new Buffer.from(bitmap, "binary").toString('base64');
-        const data = `data:image/png;base64,${img64}`;
-        appCache.set(`${id}`,data);
-        return res.json({image: data});
+            .pipe(outStream, {end: true});
+          var bitmap = await fs.readFileSync("tmp/thumbnail.png");
+          var img64 = new Buffer.from(bitmap, "binary").toString('base64');
+          const data = `data:image/png;base64,${img64}`;
+          appCache.set(`${id}`, data);
+          return res.json({image: data});
+        }
+        if (contentType === "text/html") {
+          const screenshot64 = await getPageScreenshot(url);
+          console.log(screenshot64);
+          const data = `data:image/png;base64,${screenshot64}`;
+          appCache.set(`${id}`, data);
+          return res.json({image: data});
+        } else {
+          const thumbnail = await imageThumbnail({uri: url}, {width: 320, height: 320});
+          const img64 = thumbnail.toString('base64');
+          const data = `data:image/png;base64,${img64}`;
+          appCache.set(`${id}`, data);
+          return res.json(data);
+        }
+      } catch (err) {
+        console.log(err.message)
       }
-      if(contentType === "text/html") {
-        const screenshot64 = await getPageScreenshot(url);
-        console.log(screenshot64);
-        const data = `data:image/png;base64,${screenshot64}`;
-        appCache.set(`${id}`,data);
-        return res.json({image: data});
-      }
-      else {
-        const thumbnail = await imageThumbnail({ uri: url }, {width: 320, height: 320});
-        const img64 = thumbnail.toString('base64');
-        const data = `data:image/png;base64,${img64}`;
-        appCache.set(`${id}`,data);
-        return res.json(data);
-      }
-    } catch (err) {console.log(err.message)}
+    }
+    return res.json({image: null});
+  } catch (err) {
+    return res.json({image: null, error: err})
   }
-  return res.json({image: null});
-});
+})
 
 // app.get('/stats',(req,res)=>{
 //   res.send(appCache.getStats());
